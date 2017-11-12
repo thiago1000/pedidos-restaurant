d11abf963989a4041f423a4ec3182f095b28c8f0
Thiago Viana <thiago_rallfall@hotmail.com>
2017-11-12 13:03:57 -0200

    acrescentando o projeto

---
+++ src/pages/minha-conta/minha-conta.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-minha-conta',
+  templateUrl: 'minha-conta.html'
+})
+export class MinhaContaPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
---
+++ src/pages/restaurantes/restaurantes.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-restaurantes',
+  templateUrl: 'restaurantes.html'
+})
+export class RestaurantesPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
